788ea98089df425fd01b2774198e5d1e9ad81b72
Stuart Lacy <stuart.lacy@gmail.com>
2017-11-04 13:16:25 +0000

    Added docs for col-wise functions

--- R/col_funtions.R
+++ R/col_funtions.R
@@ -10,11 +10,23 @@ build_continuous_summary <- function(func, var, digits=2) {
 
 
 
+#' Calculates the mean of a specified continuous variable
+#' for each level of an outcome.
+#'
+#' @param var A continuous variable name as a string.
+#' @return A function that calculates the mean value of \code{var}
+#' for each outcome level.
 #' @export
 summary_mean <- function(var) {
     build_continuous_summary(mean, var)
 }
 
+#' Calculates the median of a specified continuous variable
+#' for each level of an outcome.
+#'
+#' @param var A continuous variable name as a string.
+#' @return A function that calculates the median value of \code{var}
+#' for each outcome level.
 #' @export
 summary_median <- function(var) {
     build_continuous_summary(median, var)
